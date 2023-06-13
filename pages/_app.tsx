9227ef007154b81d641c0381ee880a10e5e5f1c7
AlexandreDeMoura <alexandredemoura41300@gmail.com>
2023-06-13 18:47:52 +0200

    feat: fetching data from bear API and displaying beer's data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,51 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import axios from "axios";
+import { useQuery } from "react-query";
 import styles from "../styles/Home.module.css";
 
+interface Beer {
+  id: number;
+  name: string;
+  tagline: string;
+  description: string;
+  abv: number;
+}
+
 const Home: NextPage = () => {
+  const { data, isLoading, isError } = useQuery<Beer[]>("beers", fetchBeers);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error occurred while fetching data</div>;
+  }
+
   return (
     <div className={styles.container}>
       <h1 className="text-red-600">Coucou les amis</h1>
+
+      <div>
+        <h2>Beers:</h2>
+        <ul>
+          {data?.map((beer) => (
+            <li key={beer.id}>
+              <h3>{beer.name}</h3>
+              <p>{beer.tagline}</p>
+              <p>{beer.description}</p>
+              <p>ABV: {beer.abv}%</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+};
+
+const fetchBeers = async (): Promise<Beer[]> => {
+  const response = await axios.get("https://api.punkapi.com/v2/beers");
+  return response.data;
 };
 
 export default Home;
